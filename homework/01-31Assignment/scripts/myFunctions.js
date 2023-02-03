d12eeab5b31004837318d82eec2429ae4c77ceac
GWarren2020 <74163850+GWarren2020@users.noreply.github.com>
2023-02-03 00:35:26 -0500

    add values to data fn

--- homework/01-31Assignment/scripts/myFunctions.js
+++ homework/01-31Assignment/scripts/myFunctions.js
@@ -1,7 +1,7 @@
 let data = [40, 10, 20, 60, 30];
 
 function updateData() {
-	data = [];
+	data = [40, 10, 20, 60, 30];
 	let numItems = Math.ceil(Math.random() * 10);
 	for(let i=0; i<numItems; i++) {
 		data.push(Math.random() * 800);
