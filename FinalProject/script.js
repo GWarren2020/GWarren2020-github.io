20f4bdcd2e4ad798f70e0eb40749127ee2b249b2
GWarren2020 <74163850+GWarren2020@users.noreply.github.com>
2023-02-08 01:34:23 -0500

    misc

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,39 +1,10 @@
-artists = d3.csv("https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artists.csv")
+d3.json("artists.json").then(parseLOG);
 
-artworks = d3.json("https://media.githubusercontent.com/media/MuseumofModernArt/collection/master/Artworks.json")
+function parseLOG(d) {
 
-
-lookup = ConstituentIDToObjectIDLookup = d3.merge(
-  artworks.map((d) =>
-    d.ConstituentID.map((e) => {
-      return { ConstituentID: e, ObjectID: d.ObjectID };
-    })
-  )
-)
-
-db = DuckDBClient.of([
-    FileAttachment("artworks.parquet"),
-    FileAttachment("artists@1.parquet"),
-    FileAttachment("lookup.parquet")
-  ])
-
-  
-
-  arts = getArtworks('Pablo Picasso')
-
-  Inputs.table(arts)
-
-  Plot.plot({
-    marks: [
-      Plot.dot(artistCounts, { x: "BeginDate", y: "count", r: "count", fill: "count", title: "DisplayName" }),
-    ],
-    marginTop: 50,
-    x: { line: true },
-    y: { line: true }
-  })
-
-
-
+    console.log(d);
+    
+ }
 
 
 
