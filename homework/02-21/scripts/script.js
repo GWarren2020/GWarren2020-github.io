1c0050bc69b2ccbd6305ee41cf5edc2ef2a7fe95
GWarren2020 <74163850+GWarren2020@users.noreply.github.com>
2023-02-21 20:28:13 -0500

    misc

--- homework/02-21/scripts/script.js
+++ homework/02-21/scripts/script.js
@@ -1,3 +1,25 @@
 d3.csv("data/gapminder.csv").then(function(data) {
 })
 
+// 1. define dimensions of SVG & create SVG canvas
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+console.log(window.innerWidth);
+console.log(docuent.querySelector("#chart")).clientWidth;
+
+const svg = d3.select("#chart")
+    .append
+
+// 2. filter the data
+
+// 3. determine min and max values of variables
+
+// 4. 
+
+// 5. draw axes
+
+// 6. draw bars
+
+// 7. 
